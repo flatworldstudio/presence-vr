1915be0d6427274584a69e6d82f32f935e2b974e
flatworldstudio <jasper@flatworldstudio.nl>
2018-04-23 16:29:33 +0200

    rebuild

---
+++ Assets/SteamSDK/Utilities/TargetFrameRate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetFrameRate : MonoBehaviour {
+
+	public int frameRate = 30;
+
+	// Use this for initialization
+	void Start () {
+		//Application.targetFrameRate = frameRate;
+        //QualitySettings.vSyncCount = 2;
+	}
+}
